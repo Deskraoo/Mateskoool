17c93942866c1b8ac98fffdb24b1527bfbcd634c
Vina del Mar <vina@duoc.acad>
2019-10-16 11:34:16 -0300

    cargar pagina sin fotos

--- Mateskool/blog/views.py
+++ Mateskool/blog/views.py
@@ -40,4 +40,7 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
+def inicio(request):
+    return render(request, 'blog/inicio.html')
+
 
