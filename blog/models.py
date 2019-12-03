998ffc0eb5514e03e98ae0db7747bef12923f2c7
WizardOne <wizardoneasd@gmail.com>
2019-12-02 23:15:38 -0300

    api y pruebas unitarias

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.utils.translation import ugettext as _
 from django.db import models
 from django.utils import timezone
 
@@ -17,4 +18,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        permissions = (
+            ('administrador',_('Es administrador')),
+            ('cliente',_('Es cliente')),
+        )
+
 
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse,resolve
+from blog.views import post_list,post_detail,post_new,post_edit,inicio
 
 # Create your tests here.
+
+class TestUrls(SimpleTestCase):
+
+    def test_inicio_resolves(self):
+        url=reverse('inicio')
+        self.assertEquals(resolve(url).func,inicio)
+
+    def test_post_list_resolves(self):
+        url=reverse('post_list') 
+        self.assertEquals(resolve(url).func,post_list)
+        
+    def test_post_detail_resolves(self):
+        url=reverse('post_detail') 
+        self.assertEquals(resolve(url).func,post_detail)
+
+    def test_post_new_resolves(self):
+        url=reverse('post_new') 
+        self.assertEquals(resolve(url).func,post_new)
+
+    def test_post_edit_resolved(self):
+        url=reverse('post_edit') 
+        self.assertEquals(resolve(url).func,post_edit)
